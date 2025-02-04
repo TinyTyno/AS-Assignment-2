8ebebd2945e575975c7ddbbd8e8ee3bace9b56bf
Tinytyno <wolftoast529@gmail.com>
2025-02-04 20:13:45 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AS_Assignment_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
